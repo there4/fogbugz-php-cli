1ccad5b3fb427c105e4b258a68ba3fd87208b592
Craig Davis <craig@there4development.com>
2012-09-20 08:22:07 -0500

    Add new authentication\session class, for use as the FogBugz auth.

---
+++ src/FogBugz/Authentication/Session.php
@@ -0,0 +1,21 @@
+<?php
+namespace FogBugz\Authentication;
+
+class Session
+{
+    public var $token;
+
+    public function __construct()
+    {
+    }
+
+    public function login()
+    {
+    }
+
+    public function logout()
+    {
+    }
+}
+
+/* End of file Session.php */
