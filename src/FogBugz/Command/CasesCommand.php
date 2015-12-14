b3d78e7a42fc849080913194738e136041c935fa
Mark Armendariz <src@enobrev.com>
2015-12-14 08:02:11 -0600

    Add tree argument to cases command which outputs the cases in hierarchical form

--- src/FogBugz/Command/CasesCommand.php
+++ src/FogBugz/Command/CasesCommand.php
@@ -3,6 +3,7 @@ namespace FogBugz\Command;
 
 use FogBugz\Cli\AuthCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -18,6 +19,7 @@ class CasesCommand extends AuthCommand
         $this
             ->setName('cases')
             ->setDescription('Show cases for the current filter')
+            ->addArgument('tree', InputArgument::OPTIONAL, 'Output as a hierarchy.')
             ->requireAuth(true)
             ->setHelp(
 <<<EOF
@@ -31,6 +33,7 @@ EOF
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->app = $this->getApplication();
+        $tree  = $input->getArgument('tree') == 'tree';
 
         $filterTitle = 'No filter selected';
         $xml = $this->app->fogbugz->listFilters();
@@ -43,7 +46,7 @@ EOF
 
         $xml = $this->app->fogbugz->search(
             array(
-                'cols' => 'ixBug,sStatus,sTitle,hrsCurrEst,sPersonAssignedTo'
+                'cols' => 'ixBug,ixBugParent,sStatus,sTitle,hrsCurrEst,sPersonAssignedTo'
             )
         );
 
@@ -55,17 +58,37 @@ EOF
         foreach ($xml->cases->children() as $case) {
             $data['cases'][] = array(
                 'id'           => (int) $case->ixBug,
+                'parent'       => (int) $case->ixBugParent,
                 'status'       => (string) $case->sStatus,
                 'statusFormat' => $this->app->statusStyle((string) $case->sStatus),
                 'title'        => (string) $case->sTitle,
                 'estimate'     => (string) $case->hrsCurrEst,
-                'assigned'     => (string) $case->sPersonAssignedTo
+                'assigned'     => (string) $case->sPersonAssignedTo,
+                'spaces'       => ""
             );
+        }
+
+        if ($tree) {
+            $data['cases'] = $this->parseTree($data['cases']);
         }
 
         $template = $this->app->twig->loadTemplate('cases.twig');
         $view = $template->render($data);
         $output->write($view, false, $this->app->outputFormat);
+    }
+
+    // http://stackoverflow.com/a/2915920/14651
+    protected function parseTree($tree, $root = null, $level = 0) {
+        $return = array();
+        foreach($tree as $item) {
+            if($item['parent'] == $root) {
+                unset($tree[$item['id']]);
+                $item['children'] = $this->parseTree($tree, $item['id'], $level + 1);
+                $item['spaces']   = str_repeat("   ", $level);
+                $return[] = $item;
+            }
+        }
+        return empty($return) ? null : $return;
     }
 }
 
